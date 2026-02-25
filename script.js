7c3ce147deb3b9dbc81ca54ab6522f973f048feb
Khushi <25it473@bvmengineering.ac.in>
2026-02-25 22:54:43 +0530

    JS file for calc

---
+++ script.js
@@ -0,0 +1,24 @@
+input = document.getElementById("result")
+buttons = document.querySelectorAll("button")
+output = ""
+arr = Array.from(buttons)
+arr.forEach(button => {
+    button.addEventListener("click",(e)=>{
+        if(e.target.innerHTML=="="){
+            output = eval(output)
+            input.value = output
+        }
+        else if(e.target.innerHTML=="AC"){
+            output = ""
+            input.value = output
+        }
+        else if(e.target.innerHTML=="DEL"){
+            output = output.substring(0,output.length-1);
+            input.value = output
+        }
+        else{
+            input.value = output
+            output = output + e.target.innerHTML
+        }
+    })    
+})
