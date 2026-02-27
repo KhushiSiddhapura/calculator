788606633871a2ed7be85a0f6ec676ae70b1ed35
Khushi <25it473@bvmengineering.ac.in>
2026-02-27 22:37:28 +0530

    JavaScript logic for Calculator

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
-input = document.getElementById("result")
+screen = document.getElementById("result")
 buttons = document.querySelectorAll("button")
 output = ""
 arr = Array.from(buttons)
 arr.forEach(button => {
     button.addEventListener("click",(e)=>{
-        if(e.target.innerHTML=="="){
-            output = eval(output)
-            input.value = output
-            console.log(input.value)
-        }
-        else if(e.target.innerHTML=="AC"){
+        if(button.innerText == "="){
+            output = eval(output);
+        }else if(button.innerText == "AC"){
             output = ""
-            input.value = output
-            console.log(input.value)
+        }else if(button.innerText == "DEL"){
+             output = output.substring(0,output.length-1);
+        }else{
+            output += button.innerText;
         }
-        else if(e.target.innerHTML=="DEL"){
-            output = output.substring(0,output.length-1);
-            input.value = output
-            console.log(input.value)
-        }
-        else{
-            output = output + e.target.innerHTML
-            input.value = output
-            console.log(input.value)
-        }
+        screen.value = output;
     })    
 })
